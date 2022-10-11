26cc0311a17927928373a6730b4dcef26edb23ef
Bruno Romano <brunopromano@gmail.com>
2022-10-11 00:20:39 -0300

    Adiciona persistência para entidade Lancamento

--- src/AmarisContabil.Infrastructure/Interfaces/IPersistenciaGenerica.cs
+++ src/AmarisContabil.Infrastructure/Interfaces/IPersistenciaGenerica.cs
@@ -1,6 +1,6 @@
 ﻿namespace AmarisContabil.Infrastructure.Interfaces
 {
-    internal interface IPersistenciaGenerica
+    public interface IPersistenciaGenerica
     {
         void Adicionar<T>(T entity) where T : class;
         void Atualizar<T>(T entity) where T : class;
---
+++ src/AmarisContabil.Infrastructure/LancamentoPersistencia.cs
@@ -0,0 +1,14 @@
+﻿using AmarisContabil.Infrastructure.Interfaces;
+
+namespace AmarisContabil.Infrastructure
+{
+    public class LancamentoPersistencia : PersistenciaGenerica, ILancamentoPersistencia
+    {
+        private readonly DataContext _dataContext;
+
+        public LancamentoPersistencia(DataContext dataContext) : base(dataContext)
+        {
+            _dataContext = dataContext;
+        }
+    }
+}
