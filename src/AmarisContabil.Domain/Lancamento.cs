27cd8c49e1a74df654584457c04f619345689a0f
Bruno Romano <brunopromano@gmail.com>
2022-10-10 23:39:07 -0300

    Adiciona modelo Lancamento e referência de Model no projeto Infrastructure

---
+++ src/AmarisContabil.Domain/Lancamento.cs
@@ -0,0 +1,14 @@
+﻿namespace AmarisContabil.Domain;
+
+public class Lancamento
+{
+    public int Id { get; set; }
+
+    public DateTime DataHoraInsercao { get; set; }
+    
+    public DateOnly DataLancamento { get; set; }
+    
+    public char TipoLancamento { get; set; }
+    
+    public decimal ValorBrl { get; set; }
+}
