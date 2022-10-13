0a031ebdfa20b050045741084c1acd0eb3a7c94d
Bruno Romano <brunopromano@gmail.com>
2022-10-13 01:34:08 -0300

    Adiciona estrutura para relatório consolidado por dia

---
+++ src/AmarisContabil.WebApi/Controllers/RelatorioController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AmarisContabil.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RelatorioController : Controller
+    {
+
+        public RelatorioController()
+        {
+
+        }
+
+        [HttpGet]
+        public void ObterRelatorioConsolidadoPorDia()
+        {
+
+        }
+    }
+}
