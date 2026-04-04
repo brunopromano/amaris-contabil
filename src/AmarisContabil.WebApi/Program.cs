8e5ea3e9637f487f17f401b205104c991140645d
Bruno Romano <brunopromano@gmail.com>
2026-04-04 17:41:52 -0300

    Configura logging com Serilog e adiciona persistência de logs em /app/logs; atualiza appsettings.json para refletir novas configurações de log

--- src/AmarisContabil.WebApi/Program.cs
+++ src/AmarisContabil.WebApi/Program.cs
@@ -6,8 +6,20 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((ctx, services, config) =>
+    config
+        .MinimumLevel.Information()
+        .MinimumLevel.Override("Microsoft.AspNetCore", Serilog.Events.LogEventLevel.Warning)
+        .MinimumLevel.Override("Microsoft.EntityFrameworkCore", Serilog.Events.LogEventLevel.Warning)
+        .MinimumLevel.Override("System", Serilog.Events.LogEventLevel.Warning)
+        .WriteTo.Console(outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+        .WriteTo.File(
+            path: "/app/logs/amaris-contabil.log",
+            rollingInterval: RollingInterval.Infinite,
+            outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}"));
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -30,6 +42,8 @@ var app = builder.Build();
 
 // Executa as migrações em tempo de execução
 DatabaseManagementService.MigrationInitialization(app);
+
+app.UseSerilogRequestLogging();
 
 app.UseSwagger();
 app.UseSwaggerUI();
